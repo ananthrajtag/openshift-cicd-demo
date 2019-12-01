c94b6ed6b3b442bebea58776bae6649a6ec642e7
Leandro Beretta <lberetta@redhat.com>
2019-11-30 22:18:39 -0300

    initial tekton pipeline

---
+++ demo.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+oc new-project hello-dev
+
+oc new-app -f ./application/src/main/resources/template.yaml -n hello-dev
+
+oc create -f ./pipeline/resources/hello-git.yaml -n hello-dev
+oc create -f ./pipeline/resources/hello-image.yaml -n hello-dev
+
+oc create -f ./pipeline/tasks/openshift-client.yaml -n hello-dev
+oc create -f ./pipeline/tasks/s2i-java-8.yaml -n hello-dev
+
+oc create -f ./pipeline/pipeline.yaml -n hello-dev
